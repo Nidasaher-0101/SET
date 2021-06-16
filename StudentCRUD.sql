52ddb85b8aab425a5c5e9c28666b1043dde6fa4f
Nidasaher-0101 <63342284+Nidasaher-0101@users.noreply.github.com>
2021-06-16 15:20:55 +0530

    Add files via upload

---
+++ StudentCRUD.sql
@@ -0,0 +1,24 @@
+
+
+#create table for python GUI
+CREATE TABLE TYCSE (
+	PRN VARCHAR2(10) ,
+ 	NAME VARCHAR2(20),
+ 	BATCH VARCHAR2(10)
+)
+
+/*
+# insert into table
+
+		INSERT INTO TYCSE values(25,'Revati', 'T2');
+		INSERT INTO TYCSE values(1,'Nida', 'T3');
+		INSERT INTO TYCSE values(2,'Shruti', 'T1');
+	
+# Display
+
+		Select * from TYCSE;
+# Delete from table
+		DELETE from TYCSE WHERE PRN= 25;
+
+*/
+	
